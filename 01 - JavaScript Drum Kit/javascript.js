e95a1742dc1c6d1063f29b1da2dda9cedc1f0f8b
Jaakul <jackw1689@gmail.com>
2022-08-11 18:15:37 -0700

    started layout

---
+++ 01 - JavaScript Drum Kit/javascript.js
@@ -0,0 +1,27 @@
+const key = document.querySelector('key')
+document.addEventListener("keypress",buttonFxn);
+function buttonFxn(event){
+switch (event.key){
+    case "a": document.querySelector("65").play();
+
+    case "s":
+
+    case "d":
+
+    case "f":
+
+    case "g":
+
+    case "h":
+
+    case "j":
+
+    case "k":
+
+    case "l":
+
+
+
+
+}
+}
